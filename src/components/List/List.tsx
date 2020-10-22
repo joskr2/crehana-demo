f0fdd4b78ae162b92788eb40d251d3ef380dc7c1
josue <josretamozovar@gmail.com>
2020-10-22 15:38:13 -0500

    minor update

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,90 +1,82 @@
-import React,{useEffect} from "react";
-import { useRecoilValue,useRecoilState} from "recoil";
-import { useQuery } from "@apollo/client";
-import { GET_COUNTRIES_LIST } from "../../queries/GetContriesList/GetCountriesList";
+import React, {useEffect} from "react";
+import {useRecoilValue, useRecoilState} from "recoil";
+import {useQuery} from "@apollo/client";
+import {GET_COUNTRIES_LIST} from "../../queries/GetContriesList/GetCountriesList";
 import {
-  languageState,
-  currencyState,
-  regionState,
-    setAllCountries
+    languageState,
+    currencyState,
+    regionState
 } from "../../globalRecoilState/Atoms/Atoms";
 
 const List: React.FC = () => {
-  const languageFilter = useRecoilValue<string>(languageState);
-  const currencyFilter = useRecoilValue<string>(currencyState);
-  const regionFilter = useRecoilValue<string>(regionState);
-  const [getList,setGetList] = useRecoilState(setAllCountries)
+    const languageFilter = useRecoilValue<string>(languageState);
+    const currencyFilter = useRecoilValue<string>(currencyState);
+    const regionFilter = useRecoilValue<string>(regionState);
 
-  const { data } = useQuery(GET_COUNTRIES_LIST, {
-    variables: {
-        offset: 0,
-        first: 10,
-        filter:{
-            OR: [
-                {
-                    currencies: {
-                        name: currencyFilter
+    const {data} = useQuery(GET_COUNTRIES_LIST, {
+        variables: {
+            offset: 0,
+            first: 10,
+            filter: {
+                OR: [
+                    {
+                        currencies: {
+                            name: currencyFilter
+                        }
+                    },
+                    {
+                        regionalBlocs: {
+                            name: regionFilter
+                        }
+                    },
+                    {
+                        officialLanguages: {
+                            name: languageFilter
+                        }
                     }
-                },
-                {
-                    regionalBlocs: {
-                        name: regionFilter
-                    }
-                },
-                {
-                    officialLanguages: {
-                        name: languageFilter
-                    }
-                }
-            ]
-        }
-    },
-  });
-
-    useEffect(() => {
-        let isMounted = true;
-
-        const getData = async () => {
-            if (data) {
-                return (await data) ;
-            } else {
-                return {};
+                ]
             }
-        };
-        getData()
-            .then((data: any) => {
-                if (isMounted) setGetList(data);
-            })
-            .catch();
-
-        return () => {
-            isMounted = false;
-        };
-    }, [data, setGetList]);
+        },
+    });
 
     return (
-    <table className="table-auto">
-      <thead>
-        <tr>
-          <th className="px-4 py-2">Nombre del pais</th>
-          <th className="px-4 py-2">Capital</th>
-          <th className="px-4 py-2">Bandera</th>
-        </tr>
-      </thead>
-      <tbody>
+        <>
+            {
+                languageFilter && (<table className="p-8  table-auto">
+                    <thead>
+                    <tr>
+                        <th className="px-4 py-2">Nombre del pais</th>
+                        <th className="px-4 py-2">Capital</th>
+                        <th className="px-4 py-2">Bandera</th>
+                        <th className="px-4 py-2">Moneda</th>
+                        <th className="px-4 py-2">Área</th>
+                        <th className="px-4 py-2">Denominación</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        data && data.Country.map((country: any, index: any) =>
+                            <tr key={index}>
+                                <td className="border px-4 py-2">{country.name}</td>
+                                <td className="border px-4 py-2">{country.capital}</td>
+                                <td className="border px-4 py-2"> {country &&
+                                country.flag &&
+                                country.flag.emoji}</td>
+                                <td className="border px-4 py-2">{
+                                    country &&
+                                    country.currencies &&
+                                    country.currencies[0].symbol
+                                }</td>
+                                <td className="border px-4 py-2">{country.area + " millones km²"}</td>
+                                <td className="border px-4 py-2">{country.demonym}</td>
 
-  {data&&data.Country.map((country:any, index:any)=>
-      <tr key={index}>
-          <td className="border px-4 py-2">{country.name}</td>
-          <td className="border px-4 py-2">{country.capital}</td>
-          <td className="border px-4 py-2"> {country &&
-          country.flag &&
-          country.flag.emoji}</td>
-      </tr>)}
-
-      </tbody>
-    </table>
-  );
+                            </tr>
+                        )}
+                    </tbody>
+                </table>)
+            }
+        </>
+    );
 };
 
 export default List;
--- src/queries/GetCountryInfo/GetCountryInfo.ts
+++ src/queries/GetCountryInfo/GetCountryInfo.ts
@@ -1,42 +1,42 @@
-import { gql } from "@apollo/client";
+import {gql} from "@apollo/client";
 
 const GET_COUNTRY_INFO = gql`
-  query Country($name: String!, $alpha2Code: String!) {
-    searchByName: Country(name: $name) {
-      name
-      capital
-      currencies {
-        name
-        symbol
-      }
-      area
-      populationDensity
-      nativeName
-      numericCode
-      population
-      flag {
-        svgFile
-        emoji
-      }
-    }
+    query Country($name: String!, $alpha2Code: String!) {
+        searchByName: Country(name: $name) {
+            name
+            capital
+            currencies {
+                name
+                symbol
+            }
+            area
+            populationDensity
+            nativeName
+            numericCode
+            population
+            flag {
+                svgFile
+                emoji
+            }
+        }
 
-    searchByAlpha2Code: Country(alpha2Code: $alpha2Code) {
-      name
-      capital
-      currencies {
-        name
-        symbol
-      }
-      area
-      populationDensity
-      nativeName
-      numericCode
-      population
-      flag {
-        svgFile
-        emoji
-      }
+        searchByAlpha2Code: Country(alpha2Code: $alpha2Code) {
+            name
+            capital
+            currencies {
+                name
+                symbol
+            }
+            area
+            populationDensity
+            nativeName
+            numericCode
+            population
+            flag {
+                svgFile
+                emoji
+            }
+        }
     }
-  }
 `;
-export { GET_COUNTRY_INFO };
+export {GET_COUNTRY_INFO};
